109a6aea2f77db571ece6b43ea9bc9c9722a0a3d
menjmoi <101183159+manhdzzz@users.noreply.github.com>
2026-01-06 12:58:19 +0700

    Delete Core directory

--- Core/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("BASE_URL", "/thitracnghiem/public");
-
-define("DB_HOST", "localhost");
-define("DB_NAME", "thitracnghiem");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_PORT", 3306);
