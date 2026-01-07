cc097e952fd4409a0ee29fb19d8c17fd634ecc55
menjmoi <101183159+manhdzzz@users.noreply.github.com>
2026-01-07 18:58:51 +0700

    Thêm catch lỗi kết nối csdl

--- Core/Database.php
+++ Core/Database.php
@@ -4,11 +4,15 @@ class Database
   protected $con;
   public function __construct()
   {
-    $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
-    $this->con = new PDO($dsn, DB_USER, DB_PASS, [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+    try {
+      $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+      $this->con = new PDO($dsn, DB_USER, DB_PASS, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
+    } catch (PDOException $e) {
+      die("Không thể kết nối đến cơ sở dữ liệu.");
+    }
   }
 
   public function getConnection()
